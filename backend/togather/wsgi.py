8dcb592c1e244b2c2cc5f8157062e2aa42198871
Sagun Pai <sagung.pai@gmail.com>
2015-12-11 14:44:31 +0530

    changes

--- backend/togather/wsgi.py
+++ backend/togather/wsgi.py
@@ -1,22 +1,17 @@
-"""
-WSGI config for togather project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
 import os
-
+import time
+import traceback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'togather.settings'
-
-def application(environ, start_response):
-    if environ['mod_wsgi.process_group'] != '': 
-        import signal
+try:
+    application = get_wsgi_application()
+    print 'WSGI without exception'
+except Exception:
+    print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
         os.kill(os.getpid(), signal.SIGINT)
-    return ["killed"]
-    
-application = get_wsgi_application()
+        time.sleep(2.5)
